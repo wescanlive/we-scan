2adc0f2bd6a9b3531fa846c7b1280107402531ab
wescanlive <wescanliveall@gmail.com>
2025-06-16 20:54:14 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          900: '#0f1115',
+          800: '#161b22',
+          700: '#21262d',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
